67a797ae15f545674b66d5a0f1231d9718aea82d
LFilipe03 <1211555@isep.ipp.pt>
2025-12-19 04:53:59 +0000

    Added Advance Maneuver

--- gazebo_nodes/src/controller.py
+++ gazebo_nodes/src/controller.py
@@ -5,12 +5,18 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Imu
 from nav_msgs.msg import Odometry
 from std_msgs.msg import String
+from rclpy.time import Time
 import math
 
 
 class Controller(Node):
     DRIVE = 0
-    STOPPED = 1
+    ROADWORK_DRIVE = 1
+    STOPPED = 2
+    GO_RIGHT = 3
+    GO_LEFT = 4
+    ADVANCE = 5
+    
 
     def __init__(self):
         super().__init__('controller')
@@ -38,6 +44,15 @@ class Controller(Node):
 
         self.timer = self.create_timer(0.05, self.control_loop)
 
+        self.lane_change_duration = 5  # seconds
+        self.lane_change_start_time = None
+        self.phase1_time = 4.5
+
+        self.left_yaw_offset = 0.12    # ~20°
+        self.right_yaw_offset = -0.01   # counter-steer
+
+        self.initial_yaw = None
+
         self.get_logger().info('Controller node started.')
 
     # ---------- Perception callback ----------
@@ -49,6 +64,18 @@ class Controller(Node):
                 self.get_logger().warn('STOP received from perception!')
             self.state = self.STOPPED
             self.publish_stop()
+
+        if cmd == 'ADVANCE' and self.state == self.ADVANCE: 
+            now = self.get_clock().now()
+            elapsed = (now - self.lane_change_start_time).nanoseconds * 1e-9
+            self.get_logger().info(f'Doing Advance Maneuver (elapsed: {elapsed:.2f} seconds)')
+            return
+        
+        if cmd == 'ADVANCE':
+            self.get_logger().info('ADVANCE')
+            self.state = self.ADVANCE
+            self.lane_change_start_time = self.get_clock().now()
+            self.initial_yaw = self.current_yaw
 
     # ---------- IMU callback ----------
     def imu_cb(self, msg):
@@ -75,6 +102,26 @@ class Controller(Node):
 
         if self.target_yaw is None:
             return
+
+        #Handle lane change timing
+        if self.state == self.ADVANCE:
+            now = self.get_clock().now()
+            elapsed = (now - self.lane_change_start_time).nanoseconds * 1e-9
+
+            # Phase 1: steer left
+            if elapsed < self.phase1_time:
+                self.target_yaw = self.initial_yaw + self.left_yaw_offset
+
+            # Phase 2: steer right to straighten
+            elif elapsed < self.lane_change_duration:
+                self.target_yaw = self.initial_yaw + self.right_yaw_offset
+
+            # Finish maneuver
+            else:
+                self.get_logger().info('Lane change completed, car straightened')
+                self.state = self.DRIVE
+                self.target_yaw = self.initial_yaw
+                return
 
         error = self.target_yaw - self.current_yaw
         error = math.atan2(math.sin(error), math.cos(error))
